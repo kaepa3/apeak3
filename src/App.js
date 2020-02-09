0660d6e47cb0ab127e4ec8e03d1af66d8fca27fd
kaepa3 <kaepa3@gmail.com>
2020-02-09 11:36:37 +0000

    append header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './css/App.css';
 function App() {
   return (
     <div className="App">
+      <div id="header">
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Starting Project
+          Project is start
         </p>
       </header>
     </div>
