17b5a3a1fff71bb801db15e22b95917ec30f75f1
kaepa3 <kaepa3@gmail.com>
2020-03-15 07:26:49 +0000

    delete delete.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, {Component} from 'react';
 import './css/App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Default from './Default.js';
+import Header from './Header';
+import Top from './Top.js';
+import About from './About.js';
+import Publish from './Publish.js';
 
 export default class App extends Component {
   render(){
     return (
       <BrowserRouter>
+        <Header onMyMenuClicked={this.clicked} />
+        <main className="main-content">
           <Switch>
-              <Route path="/" component={Default} />
+            <Route path="/" exact component={Top} />
+            <Route path="/about" component={About} />
+            <Route path="/publish" component={Publish} />
           </Switch>
+        </main>
       </BrowserRouter>
     );
   }
