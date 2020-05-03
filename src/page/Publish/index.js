92fb8ee308fafd6073dbaee4d2af9b32c1234228
kaepa3 <kaepa3@gmail.com>
2020-05-03 07:27:26 +0000

    create publish file

--- src/page/Publish/index.js
+++ src/page/Publish/index.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router';
-import marked from 'marked';
 import Card from '../../component/Card/index.js';
 import styles from "./styles.module.css"
 
+function createTitle (text)  {
+  var title = text.substr(0 , text.indexOf('\n'))
+  return title.replace('# ','' )
+}
+function createSubText(text){
+  var first_idx = text.indexOf('\n');
+  var second_idx = text.indexOf('\n', first_idx + 1);
+  return text.substr(first_idx, second_idx)
+}
 class Publish extends Component{
   constructor(props) {
 		super(props);
-		this.markdown = `# in preparation`
-		this.articles =[{title:"first", text:this.markdown}, {title:"second", text: this.markdown}]
+    this.state = { articles: []};
+    this.handleClick= this.handleClick.bind(this);
   }
+  componentDidMount() {
+    const readmePath = require("./hoge.md");
+    fetch(readmePath)
+      .then(response => {
+        return response.text()
+      })
+      .then(text => {
+        var title = createTitle(text)
+        var sub = createSubText(text)
+        const articles_copy = this.state.articles.slice();
+        articles_copy.push ({title: title, text:sub})
+        this.setState({
+          articles : articles_copy 
+        })
+      })
+  }
+  handleClick(e) {
+    console.log('The link was clicked.');
+    console.log(e);
+  }
+
   render(){
-    var cards = this.articles.map((value,i) => <Card key={i} {...value}></Card>)
     return(
       <div>
-        <div id="article" dangerouslySetInnerHTML={{__html:marked(this.markdown)}}></div>
-        <div className={styles.card_panel}>
-          {cards}
+        <div onClick={this.handleClick} className={styles.card_panel}>
+          {this.state.articles.map((item) => (
+            <Card onClick={this.handleClick} key={item.title} {...item} />
+           ))}
         </div>
       </div>
     );
